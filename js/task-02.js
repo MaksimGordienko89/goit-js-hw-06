4b124071cadd1fa17f3347923baabec301eec48c
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-07-24 22:26:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,30 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liArray = [];
+
+ingredients.forEach((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.className = "item";
+  liItem.textContent = ingredient;
+  liArray.push(liItem);
+});
+
+console.log(...liArray);
+// const ul = document.createElement('ul');
+
+// names.forEach((name) => {
+// const li = document.createElement('li');
+
+// li.classList = 'item';
+// li.textContent = ім'я;
+// ul.appendChild(li);
+// });
+
+// document.querySelector('body').appendChild(ul);
