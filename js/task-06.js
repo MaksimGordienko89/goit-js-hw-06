b5bd865d1d555ca5715aeb4a1530942378d30297
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-07-30 23:57:33 +0300

    task-06 help Google

---
+++ js/task-06.js
@@ -0,0 +1,10 @@
+document.getElementById("validation-input").onblur = function () {
+  if (this.getAttribute("data-length") > this.value.length) {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
+  } else {
+    this.classList.remove("invalid");
+    this.classList.add("valid");
+  }
+  console.log(this.value.length);
+};
