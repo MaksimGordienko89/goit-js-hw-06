377941f8ccc2f582cd8ad916e224311393f6d299
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-07-31 00:50:24 +0300

    tasks edit

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const changeButton = docment.querySelector("button.change-color");
-const body = docment.querySelector("body");
-const colorValue = docment.querySelector("span.color");
+
+const changeButton = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const colorValue = document.querySelector("span.color");
 
 changeButton.addEventListener("click", (e) => {
-  const color = getRandomHexColor();
+  let color = getRandomHexColor();
   body.style.backgroundColor = color;
   colorValue.textContent = color;
 });
