3f3ffb75977a37eb1a3cbad29eeca79e11f72ff0
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-01 22:09:23 +0300

    edit-task-05 and working task-10

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,20 @@ const outputName = document.querySelector("#name-output");
 inputName.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-  if (inputName.innerHTML === "") {
-    outputName.innerHTML = "Anonymous";
+  if (event.currentTarget.value === "") {
+    return (outputName.innerHTML = "Anonymous");
   }
-  outputName.innerHTML = event.currentTarget.value;
+  return (outputName.innerHTML = event.currentTarget.value);
 }
+
+// -------- Den version ------
+
+// const refs = {
+//   input: document.querySelector("#name-input"),
+//   output: document.querySelector("#name-output"),
+// };
+// refs.input.addEventListener("input", (event) => {
+//   event.currentTarget.value === ""
+//     ? (refs.output.innerHTML = "Anonymous")
+//     : (refs.output.innerHTML = event.currentTarget.value);
+// });
--- js/task-10.js
+++ js/task-10.js
@@ -1,34 +1,86 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const inputNum = document.querySelector("#controls>input");
-const btnCreate = document.querySelector("button[data-create]");
-const btnDestroy = document.querySelector("button[data-destroy]");
-const boxes = document.querySelector("#boxes");
 
-const createBoxes = (amount) => {
-  const elementsToAdd = [];
-  for (let i = 0; i < amount; i++) {
-    const div = document.createElement("div");
-    div.style.height = `${30 + 10 * i}px`;
-    div.style.width = `${30 + 10 * i}px`;
-    div.style.background = getRandomHexColor();
-    elementsToAdd.push(div);
-  }
-  return elementsToAdd;
+//--------------------my code-----------------
+
+// const inputNum = document.querySelector("#controls>input");
+// const btnCreate = document.querySelector("button[data-create]");
+// const btnDestroy = document.querySelector("button[data-destroy]");
+// const boxes = document.querySelector("#boxes");
+
+// const createBoxes = (amount) => {
+//   const elementsToAdd = [];
+//   for (let i = 0; i < amount; i++) {
+//     const div = document.createElement("div");
+//     div.style.height = `${30 + 10 * i}px`;
+//     div.style.width = `${30 + 10 * i}px`;
+//     div.style.background = getRandomHexColor();
+//     elementsToAdd.push(div);
+//   }
+//   return elementsToAdd;
+// };
+
+// const destroyBoxes = () => {
+//   boxes.innerHTML = "";
+// };
+
+// btnCreate.addEventListener("click", () => {
+//   let boxesToAdd = createBoxes(inputNum.value);
+//   boxes.append(...boxesToAdd);
+// });
+
+// console.log(inputNum.value);
+
+// btnDestroy.addEventListener("click", () => {
+//   destroyBoxes.call();
+// });
+
+//------------------Den code-------------------
+
+const refs = {
+  controls: document.querySelector("input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxInner: document.querySelector("#boxes"),
 };
+
+let inpVal = 0;
+controls.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  return (inpVal = e.target.value);
+});
+
+function createStyleOptions() {
+  let qrt = inpVal;
+  let styleArr = [];
+  const minHeight = 30;
+  const minWidth = 30;
+  for (let i = 0; i < qrt; i++) {
+    styleArr.push({
+      height: minHeight + 10 * i,
+      width: minWidth + 10 * i,
+      color: getRandomHexColor(),
+    });
+  }
+  return styleArr;
+}
+
+function createBoxes(amount) {
+  amount = createStyleOptions();
+  const newBox = amount
+    .map(
+      (box) =>
+        `<div style="width:${box.width}px; height:${box.height}px; background-color:${box.color}"></div>`
+    )
+    .join("");
+  return (refs.boxes.innerHTML = newBox);
+}
 
 const destroyBoxes = () => {
-  boxes.innerHTML = "";
+  refs.boxes.innerHTML = "";
 };
 
-btnCreate.addEventListener("click", () => {
-  let boxesToAdd = createBoxes(inputNum.value);
-  boxes.append(...boxesToAdd);
-});
+refs.btnCreate.addEventListener("click", createBoxes);
 
-console.log(inputNum.value);
-
-btnDestroy.addEventListener("click", () => {
-  destroyBoxes.call();
-});
+refs.btnDestroy.addEventListener("click", destroyBoxes);
