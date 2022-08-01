30ad79b7926db0db696c3297c34cfc0ab2ff277b
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-01 20:44:56 +0300

    edit_task-01

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,17 @@ const categoriesUl = Array.from(
   document.querySelectorAll("ul#categories>li.item")
 );
 console.log(`Number of categories: ${categoriesUl.length}`);
+
+// --- corrections after the mentor's instructions ---
 const ul = Array.from(document.querySelector("#categories").children);
-for (let elem of ul) {
+ul.forEach((elem) => {
   let category = elem.firstElementChild.textContent;
   let quantityElem = elem.lastElementChild.children.length;
   console.log(`Category: ${category} \nElements: ${quantityElem}`);
-}
+});
+// --------------------- my old code ------------------
+// for (let elem of ul) {
+//   let category = elem.firstElementChild.textContent;
+//   let quantityElem = elem.lastElementChild.children.length;
+//   console.log(`Category: ${category} \nElements: ${quantityElem}`);
+// }
