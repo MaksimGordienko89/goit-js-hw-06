b11d9c70df493767c51b3f52642c2e8fda2d7585
MaksimGordienko89 <99180492+MaksimGordienko89@users.noreply.github.com>
2022-08-02 16:48:52 +0300

    edit_task-10

--- js/task-10.js
+++ js/task-10.js
@@ -2,41 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-//--------------------my code-----------------
-
-// const inputNum = document.querySelector("#controls>input");
-// const btnCreate = document.querySelector("button[data-create]");
-// const btnDestroy = document.querySelector("button[data-destroy]");
-// const boxes = document.querySelector("#boxes");
-
-// const createBoxes = (amount) => {
-//   const elementsToAdd = [];
-//   for (let i = 0; i < amount; i++) {
-//     const div = document.createElement("div");
-//     div.style.height = `${30 + 10 * i}px`;
-//     div.style.width = `${30 + 10 * i}px`;
-//     div.style.background = getRandomHexColor();
-//     elementsToAdd.push(div);
-//   }
-//   return elementsToAdd;
-// };
-
-// const destroyBoxes = () => {
-//   boxes.innerHTML = "";
-// };
-
-// btnCreate.addEventListener("click", () => {
-//   let boxesToAdd = createBoxes(inputNum.value);
-//   boxes.append(...boxesToAdd);
-// });
-
-// console.log(inputNum.value);
-
-// btnDestroy.addEventListener("click", () => {
-//   destroyBoxes.call();
-// });
-
-//------------------Den code-------------------
+//------------------Den version-------------------
 
 const refs = {
   controls: document.querySelector("input"),
@@ -58,8 +24,8 @@ function createStyleOptions() {
   const minWidth = 30;
   for (let i = 0; i < qrt; i++) {
     styleArr.push({
-      height: minHeight + 10 * i,
-      width: minWidth + 10 * i,
+      height: minHeight + i * 10,
+      width: minWidth + i * 10,
       color: getRandomHexColor(),
     });
   }
@@ -74,13 +40,47 @@ function createBoxes(amount) {
         `<div style="width:${box.width}px; height:${box.height}px; background-color:${box.color}"></div>`
     )
     .join("");
-  return (refs.boxes.innerHTML = newBox);
+  return (refs.boxInner.innerHTML = newBox);
 }
 
 const destroyBoxes = () => {
-  refs.boxes.innerHTML = "";
+  refs.boxInner.innerHTML = "";
 };
 
 refs.btnCreate.addEventListener("click", createBoxes);
 
 refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+//--------------------my code-----------------
+
+// const inputNum = document.querySelector("#controls>input");
+// const btnCreate = document.querySelector("button[data-create]");
+// const btnDestroy = document.querySelector("button[data-destroy]");
+// const boxes = document.querySelector("#boxes");
+
+// const createBoxes = (amount) => {
+//   const elementsToAdd = [];
+//   for (let i = 0; i < amount; i++) {
+//     const divSize = 30;
+//     let growingSize = divSize + i * 10;
+//     const boxItem = `<div style="width:${growingSize}px;
+//     height:${growingSize}px; background-color:${getRandomHexColor()}" ></div>`;
+//     elementsToAdd.push(boxItem);
+//   }
+//   return (boxes.innerHTML = elementsToAdd);
+// };
+
+// const destroyBoxes = () => {
+//   boxes.innerHTML = "";
+// };
+
+// btnCreate.addEventListener("click", () => {
+//   let boxesToAdd = createBoxes(inputNum.value);
+//   boxes.append(...boxesToAdd);
+// });
+
+// console.log(inputNum.value);
+
+// btnDestroy.addEventListener("click", () => {
+//   destroyBoxes.call();
+// });
